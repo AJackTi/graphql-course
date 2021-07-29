96bc1cb693f2edfce7ac9e8782e4bfb088bbdced
Trong <dtrong97vn@gmail.com>
2021-07-29 14:53:18 +0700

    11. Operation Arguments

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -5,6 +5,8 @@ import {GraphQLServer} from 'graphql-yoga'
 // Type definitions (schema)
 const typeDefs = `
     type Query {
+        greeting(name: String, position: String): String!
+        add(a: Float!, b: Float!): Float!
         me: User!
         post: Post!
     }
@@ -27,6 +29,16 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
     Query: {
+        greeting(parent, args, ctx, info) {
+            if (args.name && args.position) {
+                return `Hello, ${args.name}! You are my favourite ${args.position}.`
+            } else {
+                return 'Hello!'
+            }
+        },
+        add(parent, args, ctx, info) {
+            return args.a + args.b
+        },
         me() {
             return {
                 id: '123098',
