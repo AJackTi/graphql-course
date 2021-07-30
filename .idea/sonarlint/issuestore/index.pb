aa437f33ac20f3c5e5e29074ac51e4d5a493ceaf
Trong <dtrong97vn@gmail.com>
2021-07-30 11:17:10 +0700

    3. Creating Data with Mutations Part II

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,8 +1,6 @@
 
 G
 graphql-basics/.babelrc,d/4/d4a2a18607e11eb3e4d691dd1536abef909e6334
-K
-graphql-basics/package.json,c/6/c6271ad097ddd603559bd2e3a7fe20b6b401679a
 P
  graphql-basics/package-lock.json,2/6/2642db0a1e933e7ea33311449cc5bf1d4702243c
 :
--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -57,7 +57,7 @@ const posts = [{
     id: '12',
     title: 'Programming Music',
     body: '',
-    published: false,
+    published: true,
     author: '2'
 }]
 
@@ -73,6 +73,8 @@ const typeDefs = `
     
     type Mutation {
         createUser(name: String!, email: String!, age: Int): User!
+        createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+        createComment(text: String!, author: ID!, post: ID!): Comment!
     }
     
     type User {
@@ -168,6 +170,44 @@ const resolvers = {
             users.push(user)
 
             return user
+        },
+        createPost(parent, args, ctx, info) {
+            const userExists = users.some((user) => user.id === args.author)
+
+            if (!userExists) {
+                throw new Error('User not found.')
+            }
+
+            const post = {
+                id: uuidv4(),
+                title: args.title,
+                body: args.body,
+                published: args.published,
+                author: args.author
+            }
+
+            posts.push(post)
+
+            return post
+        },
+        createComment(parent, args, ctx, info) {
+            const userExists = users.some((user) => user.id === args.author)
+            const postExists = posts.some((post) => post.id === args.post && post.published)
+
+            if (!userExists || !postExists) {
+                throw new Error('Unable to find user and post.')
+            }
+
+            const comment = {
+                id: uuidv4(),
+                text: args.text,
+                author: args.author,
+                post: args.post
+            }
+
+            comments.push(comment)
+
+            return comment
         }
     },
     Post: {
