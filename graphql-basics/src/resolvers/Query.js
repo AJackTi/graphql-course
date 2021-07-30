16d5fba54dbbe69fbb7def87c35ab27201435150
Trong <dtrong97vn@gmail.com>
2021-07-30 14:03:36 +0700

    9. A Pro GraphQL Project Structure Part II

---
+++ graphql-basics/src/resolvers/Query.js
@@ -0,0 +1,49 @@
+const Query = {
+    users(parent, args, {db}, info) {
+        if (!args.query) {
+            return db.users
+        }
+
+        return db.users.filter((user) => {
+            return user.name.toLowerCase().includes(args.query.toLowerCase())
+        })
+    },
+    posts(parent, args, {db}, info) {
+        if (!args.query) {
+            return db.posts
+        }
+
+        return db.posts.filter((post) => {
+            const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+            const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+
+            return isTitleMatch || isBodyMatch
+        })
+    },
+    comments(parent, args, {db}, info) {
+        if (!args.query)
+            return db.comments
+
+        return db.comments.filter((comment) => {
+            return comment.text.toLowerCase().includes(args.query.toLowerCase())
+        })
+    },
+    me() {
+        return {
+            id: '123098',
+            name: 'Mike',
+            email: 'mike@example.com',
+            age: null
+        }
+    },
+    post() {
+        return {
+            id: '092',
+            title: 'GraphQL 101',
+            body: '',
+            published: false
+        }
+    }
+}
+
+export {Query as default}
