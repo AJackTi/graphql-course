08d4b916dae8e6942be70b5fa5f7ff2a8c7cee5b
Trong <dtrong97vn@gmail.com>
2021-07-30 12:47:40 +0700

    7. Deleting Data with Mutations Part II

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -75,7 +75,9 @@ const typeDefs = `
         createUser(data: CreateUserInput!): User!
         deleteUser(id: ID!): User!
         createPost(data: CreatePostInput!): Post!
+        deletePost(id: ID!): Post!
         createComment(data: CreateCommentInput!): Comment!
+        deleteComment(id: ID!): Comment!
     }
     
     input CreateUserInput {
@@ -232,6 +234,19 @@ const resolvers = {
 
             return post
         },
+        deletePost(parent, args, ctx, info) {
+            const postIndex = posts.findIndex((post) => post.id === args.id)
+
+            if (postIndex === -1) {
+                throw new Error('Post not found.')
+            }
+
+            const deletePosts = posts.splice(postIndex, 1)
+
+            comments = comments.filter(((comment) => comment.post !== args.id))
+
+            return deletePosts[0]
+        },
         createComment(parent, args, ctx, info) {
             const userExists = users.some((user) => user.id === args.data.author)
             const postExists = posts.some((post) => post.id === args.data.post && post.published)
@@ -248,6 +263,17 @@ const resolvers = {
             comments.push(comment)
 
             return comment
+        },
+        deleteComment(parent, args, ctx, info) {
+            const commentIndex = comments.findIndex((comment) => comment.id === args.id)
+
+            if (commentIndex === -1) {
+                throw new Error('Comment not found.')
+            }
+
+            const deleteComments = comments.splice(commentIndex, 1)
+
+            return deleteComments[0]
         }
     },
     Post: {
