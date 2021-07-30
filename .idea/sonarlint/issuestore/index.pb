feb7da6ed1ca6f9c78d4109716af2f6a0ffe8305
Trong <dtrong97vn@gmail.com>
2021-07-30 10:25:56 +0700

    Complete GraphQL Basics Schemas and Queries

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+G
+graphql-basics/.babelrc,d/4/d4a2a18607e11eb3e4d691dd1536abef909e6334
+K
+graphql-basics/package.json,c/6/c6271ad097ddd603559bd2e3a7fe20b6b401679a
+P
+ graphql-basics/package-lock.json,2/6/2642db0a1e933e7ea33311449cc5bf1d4702243c
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -21,19 +21,23 @@ const users = [{
 const comments = [{
     id: '102',
     text: 'This worked well for me. Thanks!',
-    author: '3'
+    author: '3',
+    post: '10'
 }, {
     id: '103',
     text: 'Glad you enjoyed it.',
-    author: '1'
+    author: '1',
+    post: '10'
 }, {
     id: '104',
     text: 'This did no work.',
-    author: '2'
+    author: '2',
+    post: '11'
 }, {
     id: '105',
     text: 'Nevermind. I got it to work.',
-    author: '1'
+    author: '1',
+    post: '11'
 }]
 
 const posts = [{
@@ -81,12 +85,14 @@ const typeDefs = `
         body: String!
         published: Boolean!
         author: User!
+        comments: [Comment!]!
     }
     
     type Comment {
         id: ID!
         text: String!
         author: User!
+        post: Post!
     }
 `
 
@@ -144,6 +150,11 @@ const resolvers = {
             return users.find((user) => {
                 return user.id === parent.author
             })
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => {
+                return comment.post === parent.id
+            })
         }
     },
     User: {
@@ -162,6 +173,11 @@ const resolvers = {
         author(parent, args, ctx, info) {
             return users.find((user) => {
                 return user.id === parent.author
+            })
+        },
+        post(parent, args, ctx, info) {
+            return posts.find((post) => {
+                return post.id === parent.post
             })
         }
     }
