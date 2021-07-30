c7df22abb24c5d723fb61f7ded0d808d7efdf7db
Trong <dtrong97vn@gmail.com>
2021-07-30 14:17:00 +0700

    10. Updating Data with Mutations Part I

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -44,6 +44,34 @@ const Mutation = {
 
         return deleteUsers[0]
     },
+    updateUser(parent, args, {db}, info) {
+        const {id, data} = args
+        const user = db.users.find((user) => user.id === id)
+
+        if (!user) {
+            throw new Error('User not found.')
+        }
+
+        if (typeof data.email === 'string') {
+            const emailTaken = db.users.some((user) => user.email === data.email)
+
+            if (emailTaken) {
+                throw new Error('Email taken')
+            }
+
+            user.email = data.email
+        }
+
+        if (typeof data.name === 'string') {
+            user.name = data.name
+        }
+
+        if (typeof data.age !== 'undefined') {
+            user.age = data.age
+        }
+
+        return user
+    },
     createPost(parent, args, {db}, info) {
         const userExists = db.users.some((user) => user.id === args.data.author)
 
