34990cf05afa28698bcd41c22c66a42992cf2b24
Trong <dtrong97vn@gmail.com>
2021-07-30 14:29:52 +0700

    11. Updating Data with Mutations Part II

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -101,6 +101,28 @@ const Mutation = {
 
         return deletePosts[0]
     },
+    updatePost(parent, args, {db}, info) {
+        const {id, data} = args
+        const post = db.posts.find((post) => post.id === id)
+
+        if (!post) {
+            throw new Error('Post not found.')
+        }
+
+        if (typeof data.title === 'string') {
+            post.title = data.title
+        }
+
+        if (typeof data.body === 'string') {
+            post.body = data.body
+        }
+
+        if (typeof data.published === 'boolean') {
+            post.published = data.published
+        }
+
+        return post
+    },
     createComment(parent, args, {db}, info) {
         const userExists = db.users.some((user) => user.id === args.data.author)
         const postExists = db.posts.some((post) => post.id === args.data.post && post.published)
@@ -128,6 +150,20 @@ const Mutation = {
         const deleteComments = db.comments.splice(commentIndex, 1)
 
         return deleteComments[0]
+    },
+    updateComment(parent, args, {db}, info) {
+        const {id, data} = args
+        const comment = db.comments.find((comment) => comment.id === id)
+
+        if (!comment) {
+            throw new Error('Comment not found.')
+        }
+
+        if (typeof data.text === 'string') {
+            comment.text = data.text
+        }
+
+        return comment
     }
 }
 
