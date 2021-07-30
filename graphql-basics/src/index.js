067317dad3ec44f8ee6e6dd202ad205a05449d3c
Trong <dtrong97vn@gmail.com>
2021-07-30 10:49:07 +0700

    2. Creating Data with Mutations Part I

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,4 +1,5 @@
 import {GraphQLServer} from 'graphql-yoga'
+import uuidv4 from 'uuid/v4'
 
 // Scalar types - String, Boolean, Int, Float, ID
 
@@ -68,6 +69,10 @@ const typeDefs = `
         comments(query: String): [Comment!]!
         me: User!
         post: Post!
+    }
+    
+    type Mutation {
+        createUser(name: String!, email: String!, age: Int): User!
     }
     
     type User {
@@ -143,6 +148,26 @@ const resolvers = {
                 body: '',
                 published: false
             }
+        }
+    },
+    Mutation: {
+        createUser(parent, args, ctx, info) {
+            const emailTaken = users.some((user) => user.email === args.email)
+
+            if (emailTaken) {
+                throw new Error('Email taken.')
+            }
+
+            const user = {
+                id: uuidv4(),
+                name: args.name,
+                email: args.email,
+                age: args.age
+            }
+
+            users.push(user)
+
+            return user
         }
     },
     Post: {
