f61d4ddf267604d95ca49d2cf9d322e30ef82ef5
Trong <dtrong97vn@gmail.com>
2021-07-31 09:33:50 +0700

    4. Setting up a Posts Subscription

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -72,7 +72,7 @@ const Mutation = {
 
         return user
     },
-    createPost(parent, args, {db}, info) {
+    createPost(parent, args, {db, pubsub}, info) {
         const userExists = db.users.some((user) => user.id === args.data.author)
 
         if (!userExists) {
@@ -85,6 +85,8 @@ const Mutation = {
         }
 
         db.posts.push(post)
+        if (args.data.published)
+            pubsub.publish('post', {post})
 
         return post
     },
--- graphql-basics/src/resolvers/Subscription.js
+++ graphql-basics/src/resolvers/Subscription.js
@@ -17,11 +17,16 @@ const Subscription = {
         subscribe(parent, {postId}, {db, pubsub}, info) {
             const post = db.posts.find((post) => post.id === postId && post.published)
 
-            if (!post){
+            if (!post) {
                 throw new Error('Post not found')
             }
 
             return pubsub.asyncIterator(`comment ${postId}`)
+        }
+    },
+    post: {
+        subscribe(parent, args, {pubsub}, info) {
+            return pubsub.asyncIterator('post')
         }
     }
 }
