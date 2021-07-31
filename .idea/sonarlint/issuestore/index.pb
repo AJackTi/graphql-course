db18d9cd5e28e7d1f45920500baa2a0dcf4aa877
Trong <dtrong97vn@gmail.com>
2021-07-31 09:26:09 +0700

    1, 2, 3. Setting up a Comments Subscription

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -2,3 +2,7 @@
 :
 
 .gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+P
+ graphql-basics/package-lock.json,2/6/2642db0a1e933e7ea33311449cc5bf1d4702243c
+K
+graphql-basics/package.json,c/6/c6271ad097ddd603559bd2e3a7fe20b6b401679a
--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -123,7 +123,7 @@ const Mutation = {
 
         return post
     },
-    createComment(parent, args, {db}, info) {
+    createComment(parent, args, {db, pubsub}, info) {
         const userExists = db.users.some((user) => user.id === args.data.author)
         const postExists = db.posts.some((post) => post.id === args.data.post && post.published)
 
@@ -137,6 +137,7 @@ const Mutation = {
         }
 
         db.comments.push(comment)
+        pubsub.publish(`comment ${args.data.post}`, {comment: comment})
 
         return comment
     },
